fb6330aee2c5f83e375e7ad705fd42cbbc257e76
klim0v <klim0v-sergey@yandex.ru>
2019-10-29 15:06:27 +0300

    refactor

--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.13
 
 require (
 	github.com/BurntSushi/toml v0.3.0
+	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/nicksnyder/go-i18n/v2 v2.0.2
+	github.com/onsi/ginkgo v1.10.2 // indirect
+	github.com/onsi/gomega v1.7.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	golang.org/x/text v0.3.2
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/BurntSushi/toml"
+	"github.com/go-redis/redis"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
@@ -9,6 +10,7 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
+	"strconv"
 	"sync"
 	"syscall"
 )
@@ -21,7 +23,7 @@ func init() {
 	languageBundle.MustLoadMessageFile(filepath.Join(".", "locales", "actions.ru.toml"))
 }
 
-type CommandHandler struct {
+type Command struct {
 	ChatID    int64
 	Localizer *i18n.Localizer
 	Command   string
@@ -29,19 +31,26 @@ type CommandHandler struct {
 	Reply     string
 }
 
-type Chat struct {
-	lastMessage string
-	ChatID      int64
-}
-
-func (c Chat) createHandler(update tgbotapi.Update) CommandHandler {
+func formCommand(update tgbotapi.Update, rds *redis.Client) *Command {
 	if update.Message != nil {
-		return createCommandByMessage(update.Message)
+		lastCommandResult := rds.Get("lastCommand:" + strconv.Itoa(int(update.Message.Chat.ID)))
+		lastCommand, err := lastCommandResult.Result()
+		log.Println("get lastCommand", lastCommand)
+		if err != nil {
+			if err == redis.Nil {
+				lastCommand = ""
+			} else {
+				log.Println(err)
+			}
+		}
+		return createCommandByMessage(update.Message, lastCommand)
 	}
+
 	if update.CallbackQuery != nil {
 		//todo: return createCommandByCallbackQuery(update.Message)
 	}
-	return CommandHandler{}
+
+	return nil
 }
 
 func main() {
@@ -62,14 +71,16 @@ func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
+	rds := redis.NewClient(&redis.Options{Addr: os.Getenv("REDIS_ADDR")})
+	if ping := rds.Ping(); ping.Err() != nil {
+		log.Fatal(err)
+	}
+
 	var wg sync.WaitGroup
 	go func() {
 		for update := range updates {
 			wg.Add(1)
-			{
-				commandHandler := Chat{lastMessage: "todo"}.createHandler(update)
-				go handleCommand(commandHandler, bot, &wg)
-			}
+			go handle(rds, update, bot, &wg)
 		}
 	}()
 
@@ -78,10 +89,21 @@ func main() {
 	wg.Wait()
 }
 
-func createCommandByMessage(message *tgbotapi.Message) CommandHandler {
+func handle(rds *redis.Client, update tgbotapi.Update, bot *tgbotapi.BotAPI, wg *sync.WaitGroup) {
+	defer wg.Done()
+	command := formCommand(update, rds)
+	if command == nil {
+		log.Println("command is nil")
+		return
+	}
+	handleCommand(command, bot)
+	rds.SetNX("lastCommand:"+strconv.Itoa(int(command.ChatID)), command.Reply, 0)
+}
+
+func createCommandByMessage(message *tgbotapi.Message, lastCommand string) *Command {
 	chatLang := "ru"
 	log.Printf("[%s] %s", message.From.UserName, message.Text)
-	commandHandler := CommandHandler{
+	command := &Command{
 		ChatID:    message.Chat.ID,
 		Localizer: i18n.NewLocalizer(languageBundle, chatLang, message.From.LanguageCode),
 		Command:   message.Command(),
@@ -89,25 +111,32 @@ func createCommandByMessage(message *tgbotapi.Message) CommandHandler {
 		Reply:     "",
 	}
 	if !message.IsCommand() {
-		commandHandler.Command = "todo" //todo: get last bot message by message.Chat.ID from redis
-		commandHandler.Args = message.Text
+		command.Command = lastCommand
+		command.Args = message.Text
 	}
-	return commandHandler
+	return command
 }
 
-func handleCommand(commandHandler CommandHandler, bot *tgbotapi.BotAPI, wg *sync.WaitGroup) {
-	msg := commandHandler.createReplyMessage()
-	//todo: save commandHandler.Reply to redis
+func handleCommand(command *Command, bot *tgbotapi.BotAPI) {
+	msg := command.createReplyMessage()
+	if msg.ChatID == 0 {
+		log.Println("unknown command")
+		return
+	}
 	if _, err := bot.Send(msg); err != nil {
 		log.Println(err)
+		return
 	}
-	wg.Done()
 }
 
-func (command *CommandHandler) createReplyMessage() (msg tgbotapi.MessageConfig) {
+func (command *Command) createReplyMessage() tgbotapi.MessageConfig {
+	var msg tgbotapi.MessageConfig
+	log.Println("createReplyMessage:command", command.Command)
 	switch command.Command {
+	case "":
+		fallthrough
 	case "help":
-		command.Reply = "Help"
+		command.Reply = "help"
 		msg = tgbotapi.NewMessage(
 			command.ChatID,
 			command.Localizer.MustLocalize(&i18n.LocalizeConfig{MessageID: command.Reply}),
@@ -124,6 +153,8 @@ func (command *CommandHandler) createReplyMessage() (msg tgbotapi.MessageConfig)
 				tgbotapi.NewInlineKeyboardButtonData("6", "6"),
 			),
 		)
+	default:
+		return msg
 	}
-	return
+	return msg
 }
